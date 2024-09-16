171d6af84fad067ad847098bbfb126850ab6045e
Adithya N S <116829016+ADITHYA-NS@users.noreply.github.com>
2024-09-16 22:30:15 +0530

    Create D8-Reverse_words_in_string.py

---
+++ D8-Reverse_words_in_string.py
@@ -0,0 +1,28 @@
+# Day 8 
+#Reverse the words in a string
+
+def ReverseWords(s):
+    i=j=0
+    s=s.strip()
+    length=len(s)
+    temp_array=[]
+        
+    while (i<length):
+        while (i<length and s[i]==' '):
+            i+=1
+        j=i
+        while (j<length and s[j]!=' '):
+            j+=1
+        if i<j:
+            temp_array.append(s[i:j])
+        i=j
+    result=""
+    for i in range(len(temp_array)-1,-1,-1):
+        result+=temp_array[i]
+        if i!=0:
+            result+=" "
+    return result
+
+s= "the sky is blue"
+result=ReverseWords(s)
+print("Reversed Words: '"+result+"'")
