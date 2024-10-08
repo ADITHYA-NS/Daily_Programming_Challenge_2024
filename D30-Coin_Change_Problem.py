20a2d75cb91214e1366a620141578a33c6355585
Adithya N S <116829016+ADITHYA-NS@users.noreply.github.com>
2024-10-08 22:53:26 +0530

    Create D30-Coin_Change_Problem.py

---
+++ D30-Coin_Change_Problem.py
@@ -0,0 +1,18 @@
+# Day 30
+# The Coin Change Problem
+def CoinsReq(coins, amount):
+    s=sum(coins)+1
+    dp = [s] * (amount + 1)
+    dp[0] = 0
+    for coin in coins:
+        for x in range(coin, amount + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+    
+    return dp[amount] if dp[amount] != s else -1
+
+coins=[1, 2, 5]
+amount=11
+print("Coins required:",CoinsReq(coins, amount))  
+
+
+    
